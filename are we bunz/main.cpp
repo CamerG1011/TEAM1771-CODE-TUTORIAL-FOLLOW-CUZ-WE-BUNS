851418745bcc8bbf57a1589bf862b7a0777c2e0a
CamerG <74063919+CamerG1011@users.noreply.github.com>
2025-11-13 15:40:39 -0500

    king brandon is kinda gay ngl

--- are we bunz/main.cpp
+++ are we bunz/main.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main() {
-    int brandons_private_size = 3;
-    cout << "Hey yuakaka " << brandons_private_size << endl;
+    int brandon_private_size;
+    int brandon_actual_private_size;
+    int yutaka_size;
+
+    brandon_private_size = -5;
+    brandon_actual_private_size = -30;
+    yutaka_size = 1000;
+
+    cout << brandon_actual_private_size * brandon_private_size * yutaka_size << endl << "BRANDON'S GAY";
     return 0;
 }
